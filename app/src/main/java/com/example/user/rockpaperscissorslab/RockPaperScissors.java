44fd50a26df47c55dbdfc0d48e5cc65c0a009bfb
Joe Stafford <joestafford1986@gmail.com>
2018-05-30 00:34:33 +0100

    added images to buttons

--- app/src/main/java/com/example/user/rockpaperscissorslab/RockPaperScissors.java
+++ app/src/main/java/com/example/user/rockpaperscissorslab/RockPaperScissors.java
@@ -14,20 +14,9 @@ public class RockPaperScissors extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rock_paper_scissors);
-
         playerTextView = findViewById(R.id.text_player_choice);
         androidTextView = findViewById(R.id.text_android_choice);
         textResult = findViewById(R.id.text_result);
-    }
-
-    private void play(ChoiceType playerChoice) {
-        playerTextView.setText("Player's choice: " + playerChoice.toString().toLowerCase());
-
-        ChoiceType androidChoice = ChoiceType.random();
-        androidTextView.setText("Android's choice: " + androidChoice.toString().toLowerCase());
-
-        ResultType result = Game.getResult(playerChoice, androidChoice);
-        textResult.setText("Player " + result.getResult());
     }
 
     public void onMakeButtonChoice(View view) {
@@ -42,5 +31,15 @@ public class RockPaperScissors extends AppCompatActivity {
                 play(ChoiceType.SCISSORS);
                 break;
         }
+    }
+
+    private void play(ChoiceType playerChoice) {
+        playerTextView.setText("Player's choice: " + playerChoice.toString().toLowerCase());
+
+        ChoiceType androidChoice = ChoiceType.random();
+        androidTextView.setText("Android's choice: " + androidChoice.toString().toLowerCase());
+
+        ResultType result = Game.getResult(playerChoice, androidChoice);
+        textResult.setText("Player " + result.getResult());
     }
 }
